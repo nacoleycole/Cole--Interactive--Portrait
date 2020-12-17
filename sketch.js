e9b0082081bce3e0d35f5bf9546411ad9242cb34
nacoleycole <72955334+nacoleycole@users.noreply.github.com>
2020-12-17 02:49:24 -0800

    selfie button

--- sketch.js
+++ sketch.js
@@ -1,14 +1,21 @@
 let img1;
-let img2;
+let button;
+
 
 function preload(){
-  img1 = loadImage("Diptych1.png");
-  img2 = loadImage("Diptych2.png");
+  img1 = createCapture(VIDEO);//loadImage("Diptych1.png");
+ 
 }
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(10, 50, 50);
   
+   //button
+   push();
+   button = createButton('SELFFIIIIEEE!');
+   button.position(700, 450)
+   button.mousePressed(saveCanvas);
+   pop();
   
 
 }
@@ -35,36 +42,58 @@ function image1(){
 }
 
 function image2(){
-  img2.loadPixels();
+  img1.loadPixels();
   // push();
-    translate(760,0);
+    translate(500,0);
     let gridSize = 5;
      noStroke();
-    for (let y = 0; y < img2.height; y += gridSize) {
-      for (let x = 0; x < img2.width; x += gridSize) {
+    for (let y = 0; y < img1.height; y += gridSize) {
+      for (let x = 0; x < img1.width; x += gridSize) {
   
-        let index = (x + y*img2.width) * 4;
+        let index = (x + y*img1.width) * 4;
         
   
-        let r = img2.pixels[index + 2];
-        let g = img2.pixels[index + 2];
-        let b = img2.pixels[index + 2];
+        let r = img1.pixels[index + 2];
+        let g = img1.pixels[index + 2];
+        let b = img1.pixels[index + 2];
   
         fill(r, g, b);
         ellipse(x, y, gridSize, gridSize);
       }
     }
-    img2.updatePixels();
+    img1.updatePixels();
     // pop();
 
 } 
+function image3 (){
+  img1.loadPixels();
+  // push();
+    translate(480,0);
+    let gridSize = 5;
+     noStroke();
+    for (let y = 0; y < img1.height; y += gridSize) {
+      for (let x = 0; x < img1.width; x += gridSize) {
+  
+        let index = (x + y*img1.width) * 4;
+        
+  
+        let r = img1.pixels[index + 200];
+        let g = img1.pixels[index + 150];
+        let b = img1.pixels[index + 100];
+  
+        fill(r, g, b);
+        rect(x, y, gridSize, gridSize/2);
+      }
+    }
+    img1.updatePixels();
+
+}
+
 
 function draw() {
-  // push();
-   image1();
-  // pop();
-  //  push();
+  
+  image1();
   image2();
-  //  pop();
+  image3();
 
 }
